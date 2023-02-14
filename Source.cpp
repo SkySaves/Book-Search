4952fd937f500f3ef98b6b958c4c30b7b5a3099a
Zane Reisbig <zanedreisbig@yahoo.com>
2023-02-14 01:40:17 -0500

    Major Changes, full writeup in request

--- Source.cpp
+++ Source.cpp
@@ -1,147 +1,229 @@
-#include <iostream>
-#include <fstream>
 #include <vector>
 #include <string>
-#include <cstdlib>
+
 #include "Login.h"
 #include "Books.h"
+#include "MenuSystem.h"
 
+#define ISDEV 1 // Skips login prompt if set to 1 -> 0 for production
 
-using namespace std;
+// A Menu option is a class that stores the name of a menu option and the function that should be called when the option is selected
+void searchBookHandler()
+{
+    system("cls");
 
-int main() {
-    // A vector to store books
-    vector<Book> readingList;
-    // Variables to store the username and password entered by the user
+    Menu searchMenu("How would you like to search: \n");
+    searchMenu.addOption("Title", 0);
+    searchMenu.addOption("Author", 1);
+    searchMenu.addOption("ISBN", 2);
+    searchMenu.addOption("Back to main menu", 3);
+
+    string query;
+    int searchChoice;
+
+    while (true)
+    {
+        system("cls");
+
+        searchMenu.display();
+        searchMenu.getChoice(searchChoice, "Enter your search query: ");
+
+        switch (searchChoice)
+        {
+        case 0:
+            std::cout << "Enter the title of the book: ";
+            saferCin(query);
+            break;
+        case 1:
+            std::cout << "Enter the author of the book: ";
+            saferCin(query);
+            break;
+        case 2:
+            std::cout << "Enter the ISBN of the book: ";
+            saferCin(query);
+            break;
+
+        case 3:
+            return;
+
+        default:
+            break;
+        }
+
+        searchBooks(query, searchChoice);
+    }
+}
+
+void listHandler()
+{
+    system("cls");
+
+    Menu listMenu("What would you like to do: \n");
+    listMenu.addOption("Add a book to the list", 0);
+    listMenu.addOption("Remove a book from the list", 1);
+    listMenu.addOption("Display the list", 2);
+    listMenu.addOption("Back to main menu", 3);
+
+    while (true)
+    {
+        system("cls");
+
+        listMenu.display();
+        int listChoice;
+        listMenu.getChoice(listChoice);
+
+        if (listChoice == 0)
+        {
+            addToList();
+        }
+        else if (listChoice == 1)
+        {
+            removeFromList();
+        }
+        else if (listChoice == 2)
+        {
+            displayList();
+        }
+        else if (listChoice == 3)
+        {
+            break;
+        }
+        else
+        {
+            std::cout << "Invalid choice. Try again." << endl;
+        }
+    }
+}
+
+void addBookHandler()
+{
+    system("cls");
+
+    std::string bookTitle, bookAuthor, bookISBN, bookPublisher, bookDescription;
+    int bookYear;
+
+    std::cout << "Enter the title of the book: ";
+    saferCin(bookTitle);
+
+    std::cout << "Enter the author of the book: ";
+    saferCin(bookAuthor);
+
+    std::cout << "Enter the publisher of the book: ";
+    saferCin(bookPublisher);
+
+    std::cout << "Enter the year the book was published: ";
+    saferCin(bookYear);
+
+    int loopAmount = 0;
+    while (bookISBN.length() != 13)
+    {
+        if (loopAmount > 0)
+        {
+            std::cout << "ISBN was not !-13-! characters.\nPlease try again: ";
+        }
+        else
+        {
+            std::cout << "Enter the !-13-! char ISBN of the book: ";
+        }
+
+        saferCin(bookISBN);
+    }
+
+    addBook(bookISBN, bookTitle, bookAuthor, bookYear, bookPublisher);
+}
+
+int authUserHandler()
+{
+    system("cls");
+
     string username;
     string password;
-    // A variable to store the user's response when asked if they want to try again after a failed login
     string tryAgain;
-    // A variable to store the user's choice when creating a list of books
-    int listChoice;
+
+    std::cout << "Enter your username: ";
+    saferCin(username);
+
+    std::cout << "Enter your password: ";
+    saferCin(password);
+
+    while (true)
+    {
+        if (userLogin(username, password))
+        {
+            break;
+        }
+        else
+        {
+            std::cout << "Invalid login information. Try again? (y/n): " << std::endl;
+            saferCin(tryAgain);
+
+            if (tryAgain == "y")
+            {
+                std::cout << "Enter your username: " << std::endl;
+                saferCin(username);
+
+                std::cout << "Enter your password: " << std::endl;
+                saferCin(password);
+            }
+            else
+            {
+                exit(0);
+                return 0;
+            }
+        }
+    }
+
+    // Has to succeed, so we can always return 1
+    return 1;
+}
+
+int main()
+{
+    Menu mainMenu("What would you like to do: \n");
+    mainMenu.addOption("Search for a book", 0);
+    mainMenu.addOption("Add a book to the database", 1);
+    mainMenu.addOption("Create a list", 2);
+    mainMenu.addOption("Exit", 3);
 
     // Welcome message
-    cout << "Welcome to the CIS 227 Bookstore. Please enter your login information." << endl;
+    std::cout << "Welcome to the CIS 227 Bookstore. Please enter your login information." << endl;
 
-    // Prompt for username
-    cout << "Username: ";
-    cin >> username;
+    // ISDEV is defined at the top
+    if (!ISDEV)
+    {
+        authUserHandler();
+    }
 
-    // Prompt for password
-    cout << "Password: ";
-    cin >> password;
+    // Can only get here if the user is authenticated
+    // So everything after is auth safe
+    std::cout << "Reading books from file. Please wait... \n"
+              << std::endl;
+    readBooks();
 
-    // Check if the login information entered by the user is valid
-    if (userLogin(username, password)) {
-        // If the login is successful, display a message
-        cout << "Login successful. Preparing the database now..." << endl;
+    // Main Loop, switch statement is switching on return value of the menu options
+    while (true)
+    {
+        system("cls");
 
-        // Call the readBooks function to prepare the database
-        // The function is inside a try-catch block to catch exceptions that may occur during its execution
-        try {
-            readBooks();
-        }
-        catch (const exception& e) {
-            // If an exception occurs, display an error message with the exception's description
-            cout << "An error occurred while reading books: " << e.what() << endl;
-            // Exit the program with an error code
-            return 1;
-        }
-
-        // A variable to store the user's main menu choice
         int choice;
-        // Loop to continuously present the user with the main menu until they choose to exit
-        do {
-            // Main menu options
-            cout << "What would you like to do?\n1. Search for a book\n2. Add a book to the database\n3. Create a list\n4. Exit\nEnter your choice: ";
-            cin >> choice;
+        mainMenu.display();
+        mainMenu.getChoice(choice);
 
-            if (choice == 0) {
-                readBooks();
-            }
+        switch (choice)
+        {
+        case 0:
+            searchBookHandler();
+            break;
+        case 1:
+            addBookHandler();
+            break;
 
-            // If the user chooses to search for a book
-            if (choice == 1) {
-                // Clear the screen
-                system("CLS");
-                // A variable to store the search string entered by the user
-                string search;
-                // Prompt for the search string
-                cout << "Search: ";
-                cin >> search;
-                // Call the searchBooks function with the search string as an argument
-                // The function is inside a try-catch block to catch exceptions that may occur during its execution
-                try {
-                    searchBooks(search);
-                }
-                catch (const exception& e) {
-                    // If an exception occurs, display an error message with the exception's description
-                    cout << "An error occurred while searching books: " << e.what() << endl;
-                }
-            }
-            // If the user chooses to add a book to the database
-            else if (choice == 2) {
-                // Clear the screen
-                system("CLS");
-                // Call the addBook function
-                // The function is inside a try-catch block to catch exceptions that may occur during its execution
-                try {
-                    addBook();
-                }
-                catch (const exception& e) {
-                    cout << "An error occurred while adding a book: " << e.what() << endl;
-                    //This is asking the user for a choice. If the user enters 2, the program calls the function addBook(). The function addBook() is inside a try-catch block which catches exceptions if any occur during the execution of the function.
-                }
-            }
-            else if (choice == 3) {
-                system("CLS");
-                do {
-                    cout << "What would you like to do?\n1. Add a book to the list\n2. Remove a book from the list\n3. Display the list\n4. Back to main menu\nEnter your choice: ";
-                    cin >> listChoice;
-                    if (listChoice == 1) {
-                        addToList();
-                    }
-                    else if (listChoice == 2) {
-                        removeFromList();
-                    }
-                    else if (listChoice == 3) {
-                        displayList();
-                    }
-                    else if (listChoice == 4) {
-                        break;
-                    }
-                    else {
-                        cout << "Invalid choice. Try again." << endl;
-                    }
-                    readBooks();
-                } while (listChoice != 4);
-                //This is asking the user for a choice. If the user enters 3, the program enters a loop that displays a list of choices related to the reading list.The user can add books to the list, remove books from the list, display the list, or return to the main menu. The functions addToList(), removeFromList(), and displayList() are called according to the user's choice. After each choice, the function readBooks() is called.
-            }
-            else if (choice == 4) {
-                return 0;
-                //This is asking the user for a choice. If the user enters 4, the program returns 0, which exits the program. If the user enters anything else, a message is displayed and the user is prompted to try again.
-            }
-            else {
-                cout << "Invalid choice. Try again." << endl;
-                cin.ignore();
-                cin.get();
-            }
-        } while (choice != 4);
-    }
-    else {
-
-        cout << "Invalid username or password. Would you like to try again? Y/N" << endl;
-        cin >> tryAgain;
-
-        if (tryAgain == "Y") {
-            main();
+        case 2:
+            listHandler();
+            break;
+        case 3:
+            exit(0);
+            break;
         }
-
-        else {
-            return 0;
-        }
-
     }
-
-    return 0;
 }
